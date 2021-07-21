4757e7284439db6a04d126842d32679768c0ef15
Hank Lin <hank.lin@splashtop.com>
2021-07-21 18:00:43 +0800

    update 42. Trapping Rain Water

--- 42. Trapping Rain Water.js
+++ 42. Trapping Rain Water.js
@@ -1,49 +1,35 @@
 /**
- * @param {number[]} arr
- * @return {number}
- */
-const findLargestNumber = (arr) => {
-  return Math.max(...arr);
-};
-
-const countTrap = (height, startIndex, endIndex) => {
-  const startHeight = height[startIndex];
-  const endHeight = height[endIndex];
-  const basisHeight = Math.min(startHeight, endHeight);
-
-  let result = 0;
-  for (let i = startIndex + 1; i < endIndex; i += 1) {
-    result += basisHeight - height[i];
-  }
-  return result;
-};
-
-const findTrap = (height, startIndex, endIndex, maxNumIndex) => {
-  // find index that height[index] - 1 && startIndex + 1 !==  endIndex;
-  // countTrap
-  //
-
-  const maxNum = height[maxNumIndex];
-
-  return 0;
-};
-
-/**
  * @param {number[]} height
  * @return {number}
  */
 var trap = function (height) {
-  let result = 0;
-
-  const startIndex = 0;
-  const endIndex = height.length - 1;
-
-  const maxNum = findLargestNumber(height);
-  const maxNumIndex = height.indexOf(maxNum);
-
-  result += findTrap(height, startIndex, endIndex, maxNumIndex);
-
-  return result;
+  let total = 0;
+    
+  for(let i = 0; i < height.length; i += 1) {
+      let leftIdx = i;
+      let rightIdx = i;
+      let maxLeft = 0;
+      let maxRight = 0;
+      
+      while(leftIdx >= 0) {
+          maxLeft = Math.max(maxLeft, height[leftIdx]);
+          leftIdx--;
+      }
+      
+      while(rightIdx < height.length) {
+          maxRight = Math.max(maxRight, height[rightIdx]);
+          rightIdx++;
+      }
+      
+      const current = Math.min(maxLeft, maxRight) - height[i];
+      
+      
+      if (current > 0) total += current;
+      
+      
+  }
+  
+  return total;
 };
 
 // [0,1,0,2,1,0,1,3,2,1,2,1] => 6
