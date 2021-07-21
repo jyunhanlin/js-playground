3375b45d725475bcfde7e97c99095ca50ef9494c
Hank Lin <hank.lin@splashtop.com>
2021-07-21 18:22:09 +0800

    add optimized way for 42

--- 42. Trapping Rain Water.js
+++ 42. Trapping Rain Water.js
@@ -4,32 +4,59 @@
  */
 var trap = function (height) {
   let total = 0;
-    
-  for(let i = 0; i < height.length; i += 1) {
-      let leftIdx = i;
-      let rightIdx = i;
-      let maxLeft = 0;
-      let maxRight = 0;
-      
-      while(leftIdx >= 0) {
-          maxLeft = Math.max(maxLeft, height[leftIdx]);
-          leftIdx--;
-      }
-      
-      while(rightIdx < height.length) {
-          maxRight = Math.max(maxRight, height[rightIdx]);
-          rightIdx++;
-      }
-      
-      const current = Math.min(maxLeft, maxRight) - height[i];
-      
-      
-      if (current > 0) total += current;
-      
-      
+
+  for (let i = 0; i < height.length; i += 1) {
+    let leftIdx = i;
+    let rightIdx = i;
+    let maxLeft = 0;
+    let maxRight = 0;
+
+    while (leftIdx >= 0) {
+      maxLeft = Math.max(maxLeft, height[leftIdx]);
+      leftIdx--;
+    }
+
+    while (rightIdx < height.length) {
+      maxRight = Math.max(maxRight, height[rightIdx]);
+      rightIdx++;
+    }
+
+    const current = Math.min(maxLeft, maxRight) - height[i];
+
+    if (current > 0) total += current;
   }
-  
+
   return total;
+};
+
+// optimized
+const getTrappedRainwater = function (heights) {
+  let left = 0,
+    right = heights.length - 1,
+    totalWater = 0,
+    maxLeft = 0,
+    maxRight = 0;
+
+  while (left < right) {
+    if (heights[left] <= heights[right]) {
+      if (heights[left] >= maxLeft) {
+        maxLeft = heights[left];
+      } else {
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if (heights[right] >= maxRight) {
+        maxRight = heights[right];
+      } else {
+        totalWater += maxRight - heights[right];
+      }
+
+      right--;
+    }
+  }
+
+  return totalWater;
 };
 
 // [0,1,0,2,1,0,1,3,2,1,2,1] => 6
