c492d52e127c9e90defe7a7d9d7f91a0233f7d75
Hank Lin <hank.lin@splashtop.com>
2021-08-25 08:35:28 +0800

    add easy promise implementation

---
+++ web-api-implementation/MyPromise-easy.js
@@ -0,0 +1,55 @@
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+
+class Promise {
+  constructor(executor) {
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    const resolve = (value) => {
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        this.onResolvedCallbacks.forEach((fn) => fn());
+      }
+    };
+
+    const reject = (reason) => {
+      if (this.status === PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach((fn) => fn());
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.status === FULFILLED) {
+      onFulfilled(this.value);
+    }
+
+    if (this.status === REJECTED) {
+      onRejected(this.reason);
+    }
+
+    if (this.status === PENDING) {
+      this.onResolvedCallbacks.push(() => {
+        onFulfilled(this.value);
+      });
+
+      this.onRejectedCallbacks.push(() => {
+        onRejected(this.reason);
+      });
+    }
+  }
+}
