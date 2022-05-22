08d274133ad92e5833fa2d945fd3f665c942d34f
Hank Lin <jyunhanlin@gmail.com>
2022-05-22 22:34:35 +0800

    add dp problems

---
+++ leetcode/413. Arithmetic Slices.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function (A) {
+  let sum = 0,
+    dp = Array(A.length).fill(0);
+
+  for (var i = 2; i <= dp.length - 1; i++) {
+    if (A[i] - A[i - 1] === A[i - 1] - A[i - 2]) {
+      dp[i] = 1 + dp[i - 1];
+      sum += dp[i];
+    }
+  }
+
+  return sum;
+};
