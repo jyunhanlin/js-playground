cff3712ff40ffa89306d175ea6597fc03cc4041e
Hank Lin <jyunhanlin@gmail.com>
2022-07-02 09:04:54 +0800

    update dp problem

--- leetcode/746. Min Cost Climbing Stairs.js
+++ leetcode/746. Min Cost Climbing Stairs.js
@@ -17,3 +17,17 @@ const minCost = (n, cost, dp) => {
   dp[n] = cost[n] + Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp));
   return dp[n];
 };
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  const dp = [cost[0], cost[1]];
+
+  for (let i = 2; i < cost.length; i += 1) {
+    dp[i] = Math.min(cost[i] + dp[i - 2], cost[i] + dp[i - 1]);
+  }
+
+  return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+};
