468797e6d21793cb7a4a0946501b06608a4dc9fe
Hank Lin <jyunhanlin@gmail.com>
2022-08-09 08:25:39 +0800

    add calcFPS utils

---
+++ utils/calcFPS.js
@@ -0,0 +1,47 @@
+function calcFPS(fps) {
+  let tasks = [Date.now()];
+  let startTime = Date.now();
+
+  const loop = () => {
+    setTimeout(() => {
+      const current = Date.now();
+      tasks.push(current);
+      tasks = tasks.filter((time) => current - time <= 1000);
+      const time = tasks[tasks.length - 1] - tasks[0];
+
+      if (current - startTime > 1000) {
+        console.log('fps:', time ? parseInt((tasks.length / time) * 1000) : 0);
+        startTime = Date.now();
+      }
+      loop();
+    });
+  };
+  loop();
+}
+
+function calcFPS() {
+  let tasks = [Date.now()];
+  let startTime = Date.now();
+
+  const loop = () => {
+    requestAnimationFrame(() => {
+      const current = Date.now();
+      tasks.push(current);
+      const tail = tasks.length - 1;
+      if (tasks.length > 1 && tasks[tail] - tasks[tail - 1] > 1000) {
+        console.log('lag:', tasks[tail] - tasks[tail - 1]);
+      }
+
+      tasks = tasks.filter((time) => current - time <= 1000);
+      const time = tasks[tail] - tasks[0];
+
+      if (current - startTime > 1000) {
+        console.log('fps:', time ? parseInt((tasks.length / time) * 1000) : 0);
+        startTime = Date.now();
+      }
+
+      loop();
+    });
+  };
+  loop();
+}
