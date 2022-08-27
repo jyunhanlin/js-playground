974bff404c8e80cf4dbe869cf87d331ed4ec4315
Hank Lin <jyunhanlin@gmail.com>
2022-08-27 13:57:03 +0800

    update delay utils

--- utils/delay.js
+++ utils/delay.js
@@ -26,6 +26,59 @@ const createDelay =
     return delayPromise;
   };
 
+const createAbortError = () => {
+  const error = new Error('Delay aborted');
+  error.name = 'AbortError';
+  return error;
+};
+
+const createDelayWithAbort =
+  ({ willResolve }) =>
+  (ms, { value, signal } = {}) => {
+    if (signal && signal.aborted) {
+      return Promise.reject(createAbortError());
+    }
+
+    let timeoutId;
+    let settle;
+    let rejectFn;
+
+    const delayPromise = new Promise((resolve, reject) => {
+      settle = () => {
+        if (signal) {
+          signal.removeEventListener('abort', signalListener);
+        }
+        if (willResolve) {
+          resolve(value);
+        } else {
+          reject(value);
+        }
+      };
+
+      rejectFn = reject;
+      timeoutId = setTimeout(settle, ms);
+    });
+
+    if (signal) {
+      signal.addEventListener(
+        'abort',
+        () => {
+          clearTimeout(timeoutId);
+          rejectFn(createAbortError());
+        },
+        { once: true }
+      );
+    }
+
+    delayPromise.clear = () => {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+      settle();
+    };
+
+    return delayPromise;
+  };
+
 const createWithTimers = () => {
   const delay = createDelay({ willResolve: true });
   delay.reject = createDelay({ willResolve: false });
