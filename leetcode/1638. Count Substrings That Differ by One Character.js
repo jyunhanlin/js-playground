a78e39c552f5a2817e521f4e01bd53fa3483374d
Hank Lin <jyunhanlin@gmail.com>
2022-08-31 07:37:30 +0800

    update dp problem

--- leetcode/1638. Count Substrings That Differ by One Character.js
+++ leetcode/1638. Count Substrings That Differ by One Character.js
@@ -7,6 +7,36 @@ var countSubstrings = function (s, t) {
   const m = s.length;
   const n = t.length;
 
+  const dp1 = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  const dp2 = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+  let ans = 0;
+
+  for (let i = 1; i <= m; i += 1) {
+    for (let j = 1; j <= n; j += 1) {
+      if (s[i - 1] === t[j - 1]) {
+        dp1[i][j] = dp1[i - 1][j - 1] + 1;
+        dp2[i][j] = dp2[i - 1][j - 1];
+      } else {
+        dp2[i][j] = dp1[i - 1][j - 1] + 1;
+      }
+
+      ans += dp2[i][j];
+    }
+  }
+
+  return ans;
+};
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var countSubstrings = function (s, t) {
+  const m = s.length;
+  const n = t.length;
+
   const dp = new Array(m).fill(0).map(() => new Array(n).fill(0).map(() => new Array(2).fill(0)));
 
   let res = 0;
