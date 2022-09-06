af61c80c3ff1057246a7627fcd3d0f81b16b9282
Hank Lin <jyunhanlin@gmail.com>
2022-09-07 07:36:09 +0800

    add dp problem

---
+++ leetcode/877. Stone Game.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+  const n = piles.length;
+  const dp = new Array(n).fill(0).map((el) => new Array(n).fill(-Infinity));
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j >= 0; j--) {
+      if (j === i) {
+        dp[i][i] = piles[i];
+      } else {
+        dp[j][i] = Math.max(dp[j][i], piles[j] - dp[j + 1][i], piles[i] - dp[j][i - 1]);
+      }
+    }
+  }
+  return dp[0][n - 1] > 0;
+};
