84b1cfea8050bc50640b5b5cba99755adcbf91e1
Hank Lin <jyunhanlin@gmail.com>
2022-09-16 08:09:01 +0800

    add FIFOCache algorithm

---
+++ algorithm/FIFOCache.js
@@ -0,0 +1,58 @@
+class FIFOMapCache {
+  constructor({ cacheSize = 5 } = {}) {
+    this._cache = new Map();
+    this._cacheSize = cacheSize;
+  }
+  set(key, selectorFn) {
+    this._cache.set(key, selectorFn);
+
+    if (this._cache.size > this._cacheSize) {
+      const earliest = this._cache.keys().next().value;
+      this.remove(earliest);
+    }
+  }
+  get(key) {
+    return this._cache.get(key);
+  }
+  remove(key) {
+    this._cache.delete(key);
+  }
+  clear() {
+    this._cache.clear();
+  }
+}
+
+class FIFOObjectCache {
+  constructor({ cacheSize = 5 } = {}) {
+    this._cache = {};
+    this._cacheOrdering = [];
+    this._cacheSize = cacheSize;
+  }
+  set(key, selectorFn) {
+    this._cache[key] = selectorFn;
+    this._cacheOrdering.push(key);
+
+    if (this._cacheOrdering.length > this._cacheSize) {
+      const earliest = this._cacheOrdering[0];
+      this.remove(earliest);
+    }
+  }
+  get(key) {
+    return this._cache[key];
+  }
+  remove(key) {
+    const index = this._cacheOrdering.indexOf(key);
+
+    if (index > -1) {
+      this._cacheOrdering.splice(index, 1);
+    }
+    delete this._cache[key];
+  }
+  clear() {
+    this._cache = {};
+    this._cacheOrdering = [];
+  }
+  isValidCacheKey(cacheKey) {
+    return typeof cacheKey === 'string' || typeof cacheKey === 'number';
+  }
+}
