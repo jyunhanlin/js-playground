019ef804074e98c9cef743d8a10baa41e403a2ee
Hank Lin <jyunhanlin@gmail.com>
2022-09-17 09:05:42 +0800

    update dp problem

--- leetcode/1227. Airplane Seat Assignment Probability.js
+++ leetcode/1227. Airplane Seat Assignment Probability.js
@@ -7,3 +7,14 @@ var nthPersonGetsNthSeat = function (n) {
 
   return 1 / 2;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthPersonGetsNthSeat = function (n) {
+  if (n === 1) return 1;
+  return 1 / n + ((n - 2) / n) * nthPersonGetsNthSeat(n - 1);
+};
+
+// explanation: https://leetcode.com/problems/airplane-seat-assignment-probability/discuss/411905/It's-not-obvious-to-me-at-all.-Foolproof-explanation-here!!!-And-proof-for-why-it's-12
