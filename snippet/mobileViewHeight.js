46784f62a46050adf96c76e54a766847e6a8020a
Hank Lin <jyunhanlin@gmail.com>
2022-09-23 07:37:21 +0800

    update mobileViewHeight js snippet

--- snippet/mobileViewHeight.js
+++ snippet/mobileViewHeight.js
@@ -1,12 +1,15 @@
 // in css
+// :root {
+//   --app-height: 100vh;
+// }
+//
 // .my-element {
 //   height: 100vh; /* Fallback for browsers that do not support Custom Properties */
-//   height: calc(var(--vh, 1vh) * 100);
+//   height: var(--app-height);
 // }
 
 // We listen to the resize event
 window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // document.querySelector(.my-element).style.height = `${window.innerHeight}px`;
+  document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
 });
