81884d29d92f36c915465db2a9d4f1881ae082c3
Hank Lin <jyunhanlin@gmail.com>
2022-09-29 10:50:57 +0800

    add dp problem

--- leetcode/131. Palindrome Partitioning.js
+++ leetcode/131. Palindrome Partitioning.js
@@ -1,1 +1,39 @@
 // refer: https://replit.com/@ZhangMYihua/Palindrome-Partitioning#index.js
+
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {
+  const res = [];
+
+  const isPalindrome = (start, end) => {
+    while (start < end) {
+      if (s[start] !== s[end]) return false;
+
+      start++;
+      end--;
+    }
+    return true;
+  };
+
+  const helper = (startingIdx, S, partialSplits) => {
+    if (startingIdx === S.length) {
+      res.push([...partialSplits]);
+    } else {
+      for (let i = startingIdx; i < S.length; i += 1) {
+        if (isPalindrome(startingIdx, i, S)) {
+          const palindromeSnippet = S.slice(startingIdx, i + 1);
+          partialSplits.push(palindromeSnippet);
+
+          helper(i + 1, S, partialSplits);
+          partialSplits.pop();
+        }
+      }
+    }
+  };
+
+  helper(0, s, []);
+
+  return res;
+};
