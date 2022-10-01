826b4690878923ab8d555092139c369532bfd3f5
Hank Lin <jyunhanlin@gmail.com>
2022-10-01 14:58:16 +0800

    update dp problem

--- leetcode/1578. Minimum Time to Make Rope Colorful.js
+++ leetcode/1578. Minimum Time to Make Rope Colorful.js
@@ -20,3 +20,20 @@ var minCost = function (colors, neededTime) {
   res += sumCost - maxCost;
   return res;
 };
+
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function (colors, neededTime) {
+  let res = 0,
+    maxCost = 0,
+    n = colors.length;
+  for (let i = 0; i < n; ++i) {
+    if (i > 0 && colors[i] !== colors[i - 1]) maxCost = 0;
+    res += Math.min(maxCost, neededTime[i]);
+    maxCost = Math.max(maxCost, neededTime[i]);
+  }
+  return res;
+};
