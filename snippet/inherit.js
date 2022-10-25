8d438330b82c660577e010599f0db6afba6d983f
Hank Lin <jyunhanlin@gmail.com>
2022-10-25 08:00:18 +0800

    update inherit snippet

--- snippet/inherit.js
+++ snippet/inherit.js
@@ -14,3 +14,23 @@ function inherit(child, parent) {
 
   Object.setPrototypeOf(childe, parent);
 }
+
+/**
+ * @param {object} Child
+ * @param {object} Parent
+ */
+function inherit2(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+
+  Child.prototype.constructor = Child;
+
+  Child.super = Parent;
+
+  if (Object.setPrototypeOf) Object.setPrototypeOf(Child, parent);
+  else if (Child.__proto__) Child.__proto__ = Parent;
+  else {
+    for (var k in Parent) {
+      if (Parent.hasOwnProperty(k) && !(k in Child)) Child[k] = Parent[k];
+    }
+  }
+}
