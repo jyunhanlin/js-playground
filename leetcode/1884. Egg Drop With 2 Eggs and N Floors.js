979aa86ebec6f87aad591dff4b9dd5e1600be236
Hank Lin <jyunhanlin@gmail.com>
2022-10-30 07:37:58 +0800

    update egg dp problem

--- leetcode/1884. Egg Drop With 2 Eggs and N Floors.js
+++ leetcode/1884. Egg Drop With 2 Eggs and N Floors.js
@@ -28,3 +28,40 @@ var twoEggDrop = function (n) {
 var twoEggDrop = function (n) {
   return Math.ceil((Math.sqrt(1 + 8 * n) - 1) / 2);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var twoEggDrop = function (n) {
+  const dp = new Array(n + 1).fill(0).map(() => new Array(2).fill(0));
+
+  const drop = (floors, eggs) => {
+    if (eggs == 1 || floors <= 1) return floors;
+    if (dp[floors][eggs] > 0) return dp[floors][eggs];
+
+    let min = Infinity;
+    let start = 1,
+      end = floors;
+
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+      const broken = drop(mid - 1, eggs - 1);
+      const nonBroken = drop(floors - mid, eggs);
+
+      if (broken > nonBroken) {
+        end = mid - 1;
+        min = Math.min(min, broken + 1);
+      } else {
+        start = mid + 1;
+        min = Math.min(min, nonBroken + 1);
+      }
+    }
+
+    dp[floors][eggs] = min;
+
+    return min;
+  };
+
+  return drop(n, 2);
+};
