7c412b03057a4383d4c86c80420aefa300323af3
Hank Lin <jyunhanlin@gmail.com>
2022-10-30 09:41:21 +0800

    update dp problem

--- leetcode/1884. Egg Drop With 2 Eggs and N Floors.js
+++ leetcode/1884. Egg Drop With 2 Eggs and N Floors.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 var twoEggDrop = function (n) {
-  const dp = new Array(n + 1).fill(0).map(() => new Array(2).fill(0));
+  const dp = new Array(n + 1).fill(0);
 
-  const drop = (floors, eggs) => {
-    if (eggs == 1 || floors <= 1) return floors;
-    if (dp[floors][eggs] > 0) return dp[floors][eggs];
-    let min = Infinity;
-    for (let i = 1; i <= floors; i++) {
-      // breaking egg: drop(i - 1, egg - 1)
-      // non breaking egg: drop(floor - i , egg) <-- Think the i floor become the 0 floor
-      min = Math.min(min, 1 + Math.max(drop(i - 1, eggs - 1), drop(floors - i, eggs)));
+  const drop = (floors) => {
+    if (floors <= 1) return floors;
+    if (dp[floors] !== 0) return dp[floors];
+
+    let res = Infinity;
+    for (let i = 1; i <= floors; i += 1) {
+      res = Math.min(res, Math.max(i - 1, drop(floors - i)) + 1);
     }
-    dp[floors][eggs] = min;
-    return min;
+
+    dp[floors] = res;
+
+    return dp[floors];
   };
 
-  return drop(n, 2);
+  return drop(n);
 };
 
 /**
@@ -27,41 +28,4 @@ var twoEggDrop = function (n) {
  */
 var twoEggDrop = function (n) {
   return Math.ceil((Math.sqrt(1 + 8 * n) - 1) / 2);
-};
-
-/**
- * @param {number} n
- * @return {number}
- */
-var twoEggDrop = function (n) {
-  const dp = new Array(n + 1).fill(0).map(() => new Array(2).fill(0));
-
-  const drop = (floors, eggs) => {
-    if (eggs == 1 || floors <= 1) return floors;
-    if (dp[floors][eggs] > 0) return dp[floors][eggs];
-
-    let min = Infinity;
-    let start = 1,
-      end = floors;
-
-    while (start <= end) {
-      const mid = Math.floor((start + end) / 2);
-      const broken = drop(mid - 1, eggs - 1);
-      const nonBroken = drop(floors - mid, eggs);
-
-      if (broken > nonBroken) {
-        end = mid - 1;
-        min = Math.min(min, broken + 1);
-      } else {
-        start = mid + 1;
-        min = Math.min(min, nonBroken + 1);
-      }
-    }
-
-    dp[floors][eggs] = min;
-
-    return min;
-  };
-
-  return drop(n, 2);
 };
