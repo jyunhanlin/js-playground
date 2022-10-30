6e74398d57ec3422ed398935d8c549b203271ddc
Hank Lin <jyunhanlin@gmail.com>
2022-10-30 16:15:35 +0800

    update dp problem

--- leetcode/877. Stone Game.js
+++ leetcode/877. Stone Game.js
@@ -4,7 +4,7 @@
  */
 var stoneGame = function (piles) {
   const n = piles.length;
-  const dp = new Array(n).fill(0).map((el) => new Array(n).fill(-Infinity));
+  const dp = new Array(n).fill().map(() => new Array(n).fill(-Infinity));
   for (let i = 0; i < n; i++) {
     for (let j = i; j >= 0; j--) {
       if (j === i) {
@@ -15,4 +15,31 @@ var stoneGame = function (piles) {
     }
   }
   return dp[0][n - 1] > 0;
+};
+
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+  const dp = new Array(piles.length).fill().map(() => new Array(piles.length).fill(0));
+
+  const helper = (left, right, isAlice) => {
+    if (left > right) return 0;
+    if (dp[left][right]) return dp[left][right];
+
+    if (isAlice) {
+      const leftScore = helper(left + 1, right, false) + piles[left];
+      const rightScore = helper(left, right - 1, false) + piles[right];
+      dp[left][right] = Math.max(leftScore, rightScore);
+      return Math.max(leftScore, rightScore);
+    }
+
+    const leftScore = helper(left + 1, right, true) - piles[left];
+    const rightScore = helper(left, right - 1, true) - piles[right];
+    dp[left][right] = Math.min(leftScore, rightScore);
+    return Math.min(leftScore, rightScore);
+  };
+
+  return helper(0, piles.length - 1, true) > 0;
 };
