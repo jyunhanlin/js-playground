5eac98d17ed2bc7f3af6b03e79e6f6763d81da83
Hank Lin <jyunhanlin@gmail.com>
2022-10-31 08:03:34 +0800

    move inherit snippet to web api implementation

--- web-api-implementation/Prototype-inheritance.js
+++ web-api-implementation/Prototype-inheritance.js
@@ -25,3 +25,68 @@ console.log(child2.name); // daisy
 console.log(child2.age); // 20
 console.log(child2.getColors());
 
+/**
+ * @param {object} child
+ * @param {object} parent
+ */
+function inherit(child, parent) {
+  child.prototype = Object.create(parent.prototype, {
+    constructor: {
+      enumerable: false,
+      configurable: true,
+      writable: true,
+      value: parent.constructor,
+    },
+  });
+
+  Object.setPrototypeOf(childe, parent);
+}
+
+/**
+ * @param {object} Child
+ * @param {object} Parent
+ */
+function inherit2(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+
+  Child.prototype.constructor = Child;
+
+  Child.super = Parent;
+
+  if (Object.setPrototypeOf) Object.setPrototypeOf(Child, parent);
+  else if (Child.__proto__) Child.__proto__ = Parent;
+  else {
+    for (var k in Parent) {
+      if (Parent.hasOwnProperty(k) && !(k in Child)) Child[k] = Parent[k];
+    }
+  }
+}
+
+// after transpiler, the following code might fail
+class DateConstructor extends Date {
+  constructor() {
+    super();
+  }
+
+  getMyTime() {
+    return this.getTime();
+  }
+}
+
+// use the following code
+function DateConstructor() {
+  var dateObj = new (Function.prototype.bind.apply(
+    Date,
+    [Date].concat(Array.prototype.slice.call(arguments))
+  ))();
+
+  Object.setPrototypeOf(dateObj, DateConstructor.prototype);
+
+  return dateObj;
+}
+
+Object.setPrototypeOf(DateConstructor.prototype, Date.prototype);
+
+DateConstructor.prototype.getMyTime = function () {
+  return this.getTime();
+};
