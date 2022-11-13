a5efd371d8e2167d50560d333ae5ebcbf236df99
Hank Lin <jyunhanlin@gmail.com>
2022-11-14 06:52:06 +0800

    update dp problem

--- leetcode/2110. Number of Smooth Descent Periods of a Stock.js
+++ leetcode/2110. Number of Smooth Descent Periods of a Stock.js
@@ -26,3 +26,42 @@ var getDescentPeriods = function (prices) {
 
   return res;
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var getDescentPeriods = function (prices) {
+  const dp = new Array(prices.length).fill(1);
+  let res = dp[0];
+
+  for (let i = 1; i < prices.length; i += 1) {
+    if (prices[i - 1] - prices[i] === 1) dp[i] = dp[i - 1] + 1;
+    else {
+      dp[i] = 1;
+    }
+
+    res += dp[i];
+  }
+
+  return res;
+};
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var getDescentPeriods = function (prices) {
+  let pre = 1;
+  let res = 1;
+
+  for (let i = 1; i < prices.length; i += 1) {
+    if (prices[i - 1] - prices[i] === 1) pre += 1;
+    else {
+      pre = 1;
+    }
+    res += pre;
+  }
+
+  return res;
+};
