a08a2dfa6fa306014ba373f58be08918a5a97c33
Hank Lin <jyunhanlin@gmail.com>
2022-11-27 09:38:55 +0800

    add asyncPool snippet

---
+++ snippet/asyncPool.js
@@ -0,0 +1,29 @@
+async function asyncPool(poolLimit, array, iteratorFn) {
+  const ret = [];
+  const executing = [];
+  for (const item of array) {
+    const p = Promise.resolve().then(() => iteratorFn(item, array));
+    ret.push(p);
+
+    if (poolLimit <= array.length) {
+      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+      executing.push(e);
+      if (executing.length >= poolLimit) {
+        await Promise.race(executing);
+      }
+    }
+  }
+  return Promise.all(ret);
+}
+
+const timeout = (i) =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log(i);
+      resolve(i);
+    }, i)
+  );
+
+asyncPool(2, [1000, 5000, 3000, 2000], timeout).then((res) => {
+  console.log(res);
+});
