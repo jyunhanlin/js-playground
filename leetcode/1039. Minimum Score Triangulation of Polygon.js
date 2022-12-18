123079415b07a70e3ea07554f4492dfc1a08fe0c
Hank Lin <jyunhanlin@gmail.com>
2022-12-18 18:50:33 +0800

    update dp problem

--- leetcode/1039. Minimum Score Triangulation of Polygon.js
+++ leetcode/1039. Minimum Score Triangulation of Polygon.js
@@ -2,6 +2,7 @@
  * @param {number[]} values
  * @return {number}
  */
+// bottom up
 var minScoreTriangulation = function (values) {
   const len = values.length;
   const dp = new Array(len).fill().map(() => new Array(len).fill(0));
@@ -18,4 +19,26 @@ var minScoreTriangulation = function (values) {
   }
 
   return dp[0][len - 1];
+};
+
+// top down
+var minScoreTriangulation = function (values) {
+  const len = values.length;
+  const dp = new Array(len).fill().map(() => new Array(len).fill(0));
+
+  const helper = (i, j) => {
+    if (dp[i][j]) return dp[i][j];
+    let res = Infinity;
+
+    for (let k = i + 1; k < j; k += 1) {
+      res = Math.min(res, helper(i, k) + values[i] * values[k] * values[j] + helper(k, j));
+    }
+
+    if (res === Infinity) res = 0;
+
+    dp[i][j] = res;
+    return dp[i][j];
+  };
+
+  return helper(0, len - 1);
 };
