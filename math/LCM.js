29d3cd7b1a81553a324f8d518c419732423700af
Hank Lin <hank.lin@swag.live>
2022-12-24 10:47:15 +0800

    update LCM

--- math/LCM.js
+++ math/LCM.js
@@ -1,2 +1,10 @@
 // Least Common Multiple
-const LCM = (a, b) => (a * b) / GCD(a, b);
+// const LCM = (a, b) => (a * b) / GCD(a, b);
+
+const LCM = (...arr) => {
+  const gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  const _lcm = (x, y) => (x * y) / gcd(x, y);
+  return [...arr].reduce((a, b) => _lcm(a, b));
+};
+LCM(12, 7); // 84
+LCM(...[1, 3, 4, 5]); // 60
