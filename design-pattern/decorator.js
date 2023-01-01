2c988d942815224cf8432f85d779e2bff1be12eb
Hank Lin <jyunhanlin@gmail.com>
2023-01-01 09:38:23 +0800

    update decorator

--- design-pattern/decorator.js
+++ design-pattern/decorator.js
@@ -1,3 +1,31 @@
 // Structural Design Patterns
 
-// Decorator 增加
+// Decorator 增加功能
+// 重復使用基本功能
+
+var User = function (name) {
+  this.name = name;
+
+  this.say = function () {
+    console.log('User: ' + this.name);
+  };
+};
+
+var DecoratedUser = function (user, street, city) {
+  this.user = user;
+  this.name = user.name; // ensures interface stays the same
+  this.street = street;
+  this.city = city;
+
+  this.say = function () {
+    console.log('Decorated User: ' + this.name + ', ' + this.street + ', ' + this.city);
+  };
+};
+
+function run() {
+  var user = new User('Kelly');
+  user.say();
+
+  var decorated = new DecoratedUser(user, 'Broadway', 'New York');
+  decorated.say();
+}
