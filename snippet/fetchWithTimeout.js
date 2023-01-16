a948d23cf9f82b49ea6797a489e5e0ab08604d27
Hank Lin <jyunhanlin@gmail.com>
2023-01-17 07:35:39 +0800

    add fetchWithTimeout

---
+++ snippet/fetchWithTimeout.js
@@ -0,0 +1,31 @@
+async function fetchWithTimeout(url, opts = {}, timeout = 5000) {
+  // Create the AbortController instance, get AbortSignal
+  const abortController = new AbortController();
+  const { signal } = abortController;
+
+  // Make the fetch request
+  const _fetchPromise = fetch(url, {
+    ...opts,
+    signal,
+  });
+
+  // Start the timer
+  const timer = setTimeout(() => abortController.abort(), timeout);
+
+  // Await the fetch with a catch in case it's aborted which signals an error
+  try {
+    const result = await _fetchPromise;
+    clearTimeout(timer);
+    return result;
+  } catch (e) {
+    clearTimeout(timer);
+    throw e;
+  }
+}
+
+// Usage
+try {
+  const impatientFetch = await fetchWithTimeout('/', {}, 2000);
+} catch (e) {
+  console.log('fetch possibly canceled!', e);
+}
