aba1abadd73aae474074d4b521846ebda393f476
Hank Lin <jyunhanlin@gmail.com>
2023-01-21 15:49:23 +0800

    update dp problem

--- leetcode/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.js
+++ leetcode/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.js
@@ -48,3 +48,34 @@ var findTheCity = function (n, edges, distanceThreshold) {
 
   return Math.max(...res2[Math.min(...Object.keys(res2))]);
 };
+
+var findTheCity = function (n, edges, distanceThreshold) {
+  const distance = new Array(n).fill().map(() => new Array(n).fill(Infinity));
+
+  for (const edge of edges) {
+    distance[edge[0]][edge[1]] = distance[edge[1]][edge[0]] = edge[2];
+  }
+
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < n; ++j) {
+      for (let k = 0; k < n; ++k) {
+        if (k === j) continue;
+        distance[j][k] = Math.min(distance[j][k], distance[j][i] + distance[i][k]);
+      }
+    }
+  }
+
+  let city = 0;
+  let minNum = n;
+  for (let i = 0; i < n; ++i) {
+    let curNum = 0;
+    for (let j = 0; j < n; ++j) {
+      distance[i][j] <= distanceThreshold && ++curNum;
+    }
+    if (curNum <= minNum) {
+      minNum = curNum;
+      city = i;
+    }
+  }
+  return city;
+};
