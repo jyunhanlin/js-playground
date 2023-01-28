c688c5264b11881d603209e340729800b5cffa85
Hank Lin <jyunhanlin@gmail.com>
2023-01-28 09:20:38 +0800

    add iterator pattern

---
+++ design-pattern/behavioral/Iterator.js
@@ -0,0 +1,20 @@
+// Behavioral Design Patterns
+
+// Iterator
+
+class Pattern {
+  constructor(el) {
+    this.index = 0;
+    this.elements = el;
+  }
+
+  next() {
+    return this.elements[this.index++];
+  }
+
+  hasNext() {
+    return this.index < this.elements.length;
+  }
+}
+
+export default Pattern;
