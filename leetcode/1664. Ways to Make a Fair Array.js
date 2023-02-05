6f81c149765490a5081427c8c62613aae8fa1d91
Hank Lin <jyunhanlin@gmail.com>
2023-02-05 10:53:29 +0800

    update dp problem

--- leetcode/1664. Ways to Make a Fair Array.js
+++ leetcode/1664. Ways to Make a Fair Array.js
@@ -28,10 +28,6 @@ var waysToMakeFair = function (nums) {
   return res;
 };
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var waysToMakeFair = function (nums) {
   let res = 0;
   let leftOdd = 0;
@@ -55,4 +51,44 @@ var waysToMakeFair = function (nums) {
   }
 
   return res;
+};
+
+var waysToMakeFair = function (nums) {
+  if (nums.length === 1) return 1;
+  const oddSums = [];
+  const evenSums = [];
+
+  let evenSum = 0;
+  let oddSum = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    if (i % 2 === 0) {
+      evenSum += nums[i];
+      evenSums[i] = evenSum;
+    } else {
+      oddSum += nums[i];
+      oddSums[i] = oddSum;
+    }
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let preOdd, preEven, postOdd, postEven;
+
+    if (i % 2 === 0) {
+      preEven = evenSums[i - 2] || 0;
+      preOdd = oddSums[i - 1] || 0;
+      postEven = oddSums[oddSums.length - 1] - preOdd;
+      postOdd = evenSums[evenSums.length - 1] - preEven - nums[i];
+    } else {
+      preEven = evenSums[i - 1] || 0;
+      preOdd = oddSums[i - 2] || 0;
+      postEven = oddSums[oddSums.length - 1] - preOdd - nums[i];
+      postOdd = evenSums[evenSums.length - 1] - preEven;
+    }
+
+    if (preEven + postEven === preOdd + postOdd) result += 1;
+  }
+
+  return result;
 };
