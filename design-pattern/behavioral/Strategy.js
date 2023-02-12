33241c0e961bf0090c8252efdbbedfdb97c7f852
Hank Lin <jyunhanlin@gmail.com>
2023-02-13 07:45:45 +0800

    add strategy pattern

---
+++ design-pattern/behavioral/Strategy.js
@@ -0,0 +1,32 @@
+// Behavioral Design Patterns
+
+// Strategy 策略
+
+class ShoppingCart {
+  constructor(discount) {
+    this.discount = discount;
+    this.amount = 0;
+  }
+
+  checkout() {
+    return this.discount(this.amount);
+  }
+
+  setAmount(amount) {
+    this.amount = amount;
+  }
+}
+
+function guestPattern(amount) {
+  return amount;
+}
+
+function regularPattern(amount) {
+  return amount * 0.9;
+}
+
+function premiumPattern(amount) {
+  return amount * 0.8;
+}
+
+export { ShoppingCart, guestPattern, regularPattern, premiumPattern };
