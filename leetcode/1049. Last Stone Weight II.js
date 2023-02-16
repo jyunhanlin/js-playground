d2d9ddbd5dffc5f03e863bd3dee1ee866c9e5a42
Hank Lin <jyunhanlin@gmail.com>
2023-02-16 08:05:21 +0800

    add 1049. Last Stone Weight II (WIP)

---
+++ leetcode/1049. Last Stone Weight II.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeightII = function (stones) {
+  let result = Infinity;
+  const helper = (remainStones) => {
+    if (remainStones.length <= 1) {
+      const last = remainStones[0] || 0;
+      result = Math.min(result, last);
+    } else {
+      for (let i = 0; i < remainStones.length - 1; i += 1) {
+        for (let j = i + 1; j < remainStones.length; j += 1) {
+          const newStones = [...remainStones];
+          if (j > i) {
+            if (newStones[i] === newStones[j]) {
+              newStones.splice(j, 1);
+              newStones.splice(i, 1);
+              helper(newStones);
+            } else if (newStones[i] < newStones[j]) {
+              newStones.splice(i, 1, newStones[j] - newStones[i]);
+              newStones.splice(j, 1);
+              helper(newStones);
+            } else if (newStones[i] > newStones[j]) {
+              newStones.splice(j, 1, newStones[i] - newStones[j]);
+              newStones.splice(i, 1);
+              helper(newStones);
+            }
+          }
+        }
+      }
+    }
+  };
+
+  helper(stones);
+
+  return result;
+};
