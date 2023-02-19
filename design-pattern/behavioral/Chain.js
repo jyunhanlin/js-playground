be99918b82e1c4be4bb77ae2d17e9e755d0a3f38
Hank Lin <jyunhanlin@gmail.com>
2023-02-20 07:47:44 +0800

    update chain pattern

--- design-pattern/behavioral/Chain.js
+++ design-pattern/behavioral/Chain.js
@@ -20,3 +20,76 @@ function run() {
 
   request.get(100).get(50).get(20).get(10).get(5).get(1);
 }
+
+// -----
+
+class ShoppingCart {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(p) {
+    this.products.push(p);
+  }
+}
+
+class Discount {
+  calc(products) {
+    let ndiscount = new NumberDiscount();
+    let pdiscount = new PriceDiscount();
+    let none = new NoneDiscount();
+    ndiscount.setNext(pdiscount);
+    pdiscount.setNext(none);
+
+    return ndiscount.exec(products);
+  }
+}
+
+class NumberDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  }
+
+  exec(products) {
+    let result = 0;
+    if (products.length > 3) result = 0.05;
+
+    return result + this.next.exec(products);
+  }
+}
+
+class PriceDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  }
+
+  exec(products) {
+    let result = 0;
+    let total = products.reduce((a, b) => a + b);
+
+    if (total >= 500) result = 0.1;
+
+    return result + this.next.exec(products);
+  }
+}
+
+class NoneDiscount {
+  exec() {
+    return 0;
+  }
+}
+
+const cart = new ShoppingCart();
+cart.addProduct(1);
+cart.addProduct(2);
+cart.addProduct(3);
+const discount = new Discount();
+const totalPrice = discount.calc(cart.products);
