ec66b8d38851bd5352510cd5509995502b2e4efa
Hank Lin <hank.lin@swag.live>
2023-04-12 18:55:09 +0800

    update retryWithBackoff to promiseRetry

---
+++ snippet/promiseRetry.js
@@ -0,0 +1,52 @@
+/**
+ * Wait for the given milliseconds
+ * @param {number} milliseconds The given time to wait
+ * @returns {Promise} A fulfilled promise after the given time has passed
+ */
+function waitFor(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+/**
+ * Execute a promise and retry with exponential backoff
+ * based on the maximum retry attempts it can perform
+ * @param {object} options
+ * @param {() => Promise | Promise} options.promise
+ * @param {(retries: number) => number | number} [options.getNextTimeToWait]
+ * @param {(retries: number) => Promise} [options.onRetry]
+ * @param {(error: any, retries: number) => Promise} [options.onError]
+ * @param {number} [options.maxRetries=3]
+ * @return {Promise}
+ */
+function promiseRetry({
+  promise,
+  getNextTimeToWait = (retries) => 2 ** retries * 1000,
+  onRetry,
+  onError,
+  maxRetries = 3,
+}) {
+  async function retry(retries) {
+    try {
+      if (retries > 0) {
+        let timeToWait = getNextTimeToWait;
+        if (typeof getNextTimeToWait === 'function') timeToWait = getNextTimeToWait(retries);
+        console.log(`waiting for ${timeToWait}ms...`);
+        await waitFor(timeToWait);
+      }
+
+      if (typeof promise === 'function') return await promise();
+
+      return await promise;
+    } catch (e) {
+      if (retries < maxRetries) {
+        onRetry?.(retries);
+        return retry(retries + 1);
+      } else {
+        onError?.(error, retries);
+        throw e;
+      }
+    }
+  }
+
+  return retry(0);
+}
