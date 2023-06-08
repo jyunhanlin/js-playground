d7b1ef6c933afce6c60bade62f4b8ec65da36167
Hank Lin <hank.lin@swag.live>
2023-06-08 11:04:57 +0800

    add floydWarshall algorithm

---
+++ algorithm/floydWarshall.js
@@ -0,0 +1,32 @@
+// shortestPath(i, j, k) = min (shortestPath(i, j, k - 1), shortestPath(i, k, k - 1) + shortestPath(k, j, k - 1))
+
+function init(graph) {
+  const dist = [];
+  const len = graph.length;
+  for (let i = 0; i < len; i += 1) {
+    dist[i] = [];
+    for (let j = 0; j < len; j += 1) {
+      if (i === j) {
+        dist[i][j] = 0;
+      } else if (!isFinite(graph[i][j])) {
+        dist[i][j] = Infinity;
+      } else {
+        dist[i][j] = graph[i][j];
+      }
+    }
+  }
+  return dist;
+}
+
+function floydWarshall(graph) {
+  const dist = init(graph);
+  const len = graph.length;
+  for (let k = 0; k < len; k += 1) {
+    for (let i = 0; i < len; i += 1) {
+      for (let j = 0; j < len; j += 1) {
+        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+      }
+    }
+  }
+  return dist;
+}
