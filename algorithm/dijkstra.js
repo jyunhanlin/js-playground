d8e2fe729112ad03b448ea612ce55187db44de2f
Hank Lin <jyunhanlin@gmail.com>
2023-06-10 09:27:06 +0800

    add dijkstra

---
+++ algorithm/dijkstra.js
@@ -0,0 +1,43 @@
+// found the shortest path between two nodes
+// common variant: fixes a single node as the "source" node and finds shortest paths from the source to all other nodes in the graph
+
+let vertices = [];
+let adjacencyList = null;
+
+function dijkstra(startNode) {
+  const distances = {};
+  const paths = {};
+  const visited = new Set();
+
+  // TODO: build the adjacencyList from vertices
+
+  // Build the initial dp object from vertices
+  for (let i = 0; i < vertices.length; i++) {
+    if (vertices[i] === startNode) {
+      distances[startNode] = 0;
+    } else {
+      distances[vertices[i]] = Infinity;
+    }
+    paths[vertices[i]] = null;
+  }
+
+  let currVertex = vertexWithMinDistance(distances, visited); // can use priority queue
+
+  while (currVertex !== null) {
+    const distance = distances[currVertex];
+    const neighbors = adjacencyList[currVertex];
+
+    for (let neighbor in neighbors) {
+      let newDistance = distance + neighbors[neighbor];
+      if (distances[neighbor] > newDistance) {
+        distances[neighbor] = newDistance;
+        paths[neighbor] = currVertex;
+      }
+    }
+    visited.add(currVertex);
+    currVertex = vertexWithMinDistance(distances, visited);
+  }
+
+  console.log(paths);
+  console.log(distances);
+}
