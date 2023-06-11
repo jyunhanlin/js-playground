20c5eb764c5b7fe8e6a78981134e7f7fbe188b6f
Hank Lin <jyunhanlin@gmail.com>
2023-06-11 11:20:28 +0800

    add BellmanFord

---
+++ algorithm/BellmanFord.js
@@ -0,0 +1,33 @@
+// find shortest paths from a single source vertex to all of the other vertices in a weighted digraph.
+// slower than Dijkstra's algorithm
+// handle graphs in which some of the edge weights are negative numbers.
+
+// Time complexity: Worst-case performance O(|V||E|) Best-case performance O(|E|)
+// Space complexity: O(|V|)
+
+/**
+ * @param {number} vertexNumber
+ * @param {array} edges (u -> v, the weight is w)
+ * @param {number} startVertex
+ * @return {array} dist
+ */
+const bellman_ford = (vertexNumber, edges, startVertex) => {
+  const dist = new Array(vertexNumber - 1).fill(Infinity);
+  dist[startVertex] = 0;
+
+  for (let i = 0; i < vertexNumber - 1; i++) {
+    for (let [u, v, w] of edges) {
+      if (dist[u] !== Infinity && dist[u] + w < dist[v]) {
+        dist[v] = dist[u] + w;
+      }
+    }
+  }
+  // nth relaxation to check negative cycle
+  for (let [u, v, w] of edges) {
+    if (dist[u] !== Infinity && dist[u] + w < dist[v]) {
+      return [-1];
+    }
+  }
+
+  return dist;
+};
