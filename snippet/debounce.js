8c4d574fcc6f7c56d71dba7c97c73444dd871a7c
Hank Lin <jyunhanlin@gmail.com>
2023-10-28 07:33:12 +0800

    update debouncePromise in debounce

--- snippet/debounce.js
+++ snippet/debounce.js
@@ -42,3 +42,32 @@ function debounce(fn, delay) {
 
   return debounced;
 }
+
+const debouncePromise = (fn, ms = 0) => {
+  let timeoutId;
+  const pending = [];
+  return (...args) =>
+    new Promise((res, rej) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        const currentPending = [...pending];
+        pending.length = 0;
+        Promise.resolve(fn.apply(this, args)).then(
+          (data) => {
+            currentPending.forEach(({ resolve }) => resolve(data));
+          },
+          (error) => {
+            currentPending.forEach(({ reject }) => reject(error));
+          }
+        );
+      }, ms);
+      pending.push({ resolve: res, reject: rej });
+    });
+};
+const fn = (arg) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000, ['resolved', arg]);
+  });
+const debounced = debouncePromise(fn, 200);
+debounced('foo').then(console.log);
+debounced('bar').then(console.log);
