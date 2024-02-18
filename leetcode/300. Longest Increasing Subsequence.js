adae5b8d8e1484ba3a6df8496c3b69cecbaabf7b
Hank Lin <jyunhanlin@gmail.com>
2024-02-18 20:51:57 +0800

    update dp problem

--- leetcode/300. Longest Increasing Subsequence.js
+++ leetcode/300. Longest Increasing Subsequence.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  var lis = [];
-  for (var i = 0; i < nums.length; i++) {
+  const lis = [];
+  for (let i = 0; i < nums.length; i += 1) {
     lis.push(1);
-    for (var j = 0; j < i; j++) {
+    for (let j = 0; j < i; j++) {
       if (nums[j] < nums[i]) lis[i] = Math.max(lis[i], lis[j] + 1);
     }
   }
