14ede1710e366fcbaa4b8135c237893f8c84c94b
Hank Lin <jyunhanlin@gmail.com>
2024-04-13 21:43:46 +0800

    update dp problem

--- leetcode/474. Ones and Zeroes.js
+++ leetcode/474. Ones and Zeroes.js
@@ -30,3 +30,37 @@ var findMaxForm = function (strs, m, n) {
 
   return dp[m][n];
 };
+
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function (strs, m, n) {
+  const dp = Array(strs.length + 1)
+    .fill()
+    .map(() =>
+      Array(m + 1)
+        .fill()
+        .map(() => Array(n + 1).fill(0))
+    );
+
+  for (let i = 1; i <= strs.length; i++) {
+    let count0 = 0;
+    let count1 = 0;
+    for (const str of strs[i - 1]) {
+      str === '1' ? count1++ : count0++;
+    }
+    for (let z = 0; z <= m; z++) {
+      for (let o = 0; o <= n; o++) {
+        dp[i][z][o] = Math.max(
+          z >= count0 && o >= count1 ? 1 + dp[i - 1][z - count0][o - count1] : 0,
+          dp[i - 1][z][o]
+        );
+      }
+    }
+  }
+
+  return dp[strs.length][m][n];
+};
