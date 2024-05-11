11127b719b2a7c9a41b5cb4556fb3fb5f0147e5a
Hank Lin <jyunhanlin@gmail.com>
2024-05-11 21:22:59 +0800

    update dp problem

--- leetcode/1186. Maximum Subarray Sum with One Deletion.js
+++ leetcode/1186. Maximum Subarray Sum with One Deletion.js
@@ -22,3 +22,34 @@ var maximumSum = function (arr) {
 
   return max;
 };
+
+// Time Limit Exceeded
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumSum = function (arr) {
+  const dp1 = new Array(arr.length).fill(-Infinity);
+  const dp2 = new Array(arr.length).fill(-Infinity);
+
+  let max = Math.max(...arr);
+  dp1[0] = arr[0];
+
+  for (let i = 1; i < arr.length; i += 1) {
+    dp1[i] = Math.max(dp1[i - 1] + arr[i], arr[i]);
+
+    max = Math.max(max, dp1[i]);
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const newArr = arr.filter((_, index) => index !== i);
+    dp2[0] = newArr[0];
+    for (let j = 1; j < newArr.length; j += 1) {
+      dp2[j] = Math.max(dp2[j - 1] + newArr[j], newArr[j]);
+
+      max = Math.max(max, dp2[j]);
+    }
+  }
+
+  return max;
+};
