822553b7dda05183ea4a3868766dd7c99e6bf883
Hank Lin <jyunhanlin@gmail.com>
2024-06-10 22:23:21 +0800

    update dp

--- leetcode/2826. Sorting Three Groups.js
+++ leetcode/2826. Sorting Three Groups.js
@@ -21,3 +21,21 @@ var minimumOperations = function (nums) {
 
   return n - Math.max(...dp);
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function (nums) {
+  const n = nums.length;
+
+  const lis = [];
+  for (let i = 0; i < nums.length; i += 1) {
+    lis.push(1);
+    for (let j = 0; j < i; j++) {
+      if (nums[j] <= nums[i]) lis[i] = Math.max(lis[i], lis[j] + 1);
+    }
+  }
+
+  return n - Math.max(...lis);
+};
