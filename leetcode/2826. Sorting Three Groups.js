0a27905b6996b340f54559f77da001f28c1369ac
Hank Lin <jyunhanlin@gmail.com>
2024-06-10 22:23:44 +0800

    update dp

--- leetcode/2826. Sorting Three Groups.js
+++ leetcode/2826. Sorting Three Groups.js
@@ -33,7 +33,7 @@ var minimumOperations = function (nums) {
   for (let i = 0; i < nums.length; i += 1) {
     lis.push(1);
     for (let j = 0; j < i; j++) {
-      if (nums[j] <= nums[i]) lis[i] = Math.max(lis[i], lis[j] + 1);
+      if (nums[i] >= nums[j]) lis[i] = Math.max(lis[i], lis[j] + 1);
     }
   }
 
