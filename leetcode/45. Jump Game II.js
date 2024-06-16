59d7c4ecec5f795575a5daf566ba0ae688e0b2c5
Hank Lin <jyunhanlin@gmail.com>
2024-06-16 14:54:21 +0800

    update dp

--- leetcode/45. Jump Game II.js
+++ leetcode/45. Jump Game II.js
@@ -2,12 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-const jump = function (nums) {
-  const finalIndex = nums.length - 1;
-  let steps = [];
+var jump = function (nums) {
+  const dp = new Array(nums.length).fill(Infinity);
 
-  // find closest num to finalIndex;
-  //
+  dp[0] = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const steps = nums[i];
+    for (let j = 0; j <= steps; j += 1) {
+      const k = i + j < nums.length ? i + j : nums.length - 1;
+
+      dp[k] = Math.min(dp[k], dp[i] + 1);
+    }
+  }
+
+  return dp[nums.length - 1];
 };
 
 // Input: nums = [2,3,1,1,4]
