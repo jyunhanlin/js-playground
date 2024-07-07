24908761e92dfdc5c76c61b878ebd7f2dea36a7e
Hank Lin <jyunhanlin@gmail.com>
2024-07-07 16:33:16 +0800

    update dp problem

--- leetcode/787. Cheapest Flights Within K Stops.js
+++ leetcode/787. Cheapest Flights Within K Stops.js
@@ -8,10 +8,9 @@
  */
 var findCheapestPrice = function (n, flights, src, dst, k) {
   // BFS and adjacency list
-
   const adj = {};
-  const visited = new Array(n).fill(Infinity);
-  visited[src] = 0;
+  const dp = new Array(n).fill(Infinity);
+  dp[src] = 0;
 
   for (let i = 0; i < flights.length; i += 1) {
     const [from, to, price] = flights[i];
@@ -19,24 +18,26 @@ var findCheapestPrice = function (n, flights, src, dst, k) {
     adj[from].push([to, price]);
   }
 
-  const queue = [[src, 0]];
+  const queue = [[[src, 0]]];
   while (k >= 0 && queue.length > 0) {
     k -= 1;
-    const size = queue.length;
-    for (let i = 0; i < size; i += 1) {
-      const [curNode, curPrice] = queue.shift();
+    const curAdj = queue.shift();
+    const newAdj = [];
+    for (let i = 0; i < curAdj.length; i += 1) {
+      const [curNode, curPrice] = curAdj[i];
       if (adj[curNode]) {
         for (let j = 0; j < adj[curNode].length; j += 1) {
           const [nextNode, nextPrice] = adj[curNode][j];
           const newPrice = curPrice + nextPrice;
-          if (newPrice < visited[nextNode]) {
-            visited[nextNode] = newPrice;
-            queue.push([nextNode, newPrice]);
+          if (newPrice < dp[nextNode]) {
+            dp[nextNode] = newPrice;
+            newAdj.push([nextNode, newPrice]);
           }
         }
       }
     }
+    if (newAdj.length) queue.push(newAdj);
   }
 
-  return visited[dst] === Infinity ? -1 : visited[dst];
+  return dp[dst] === Infinity ? -1 : dp[dst];
 };
