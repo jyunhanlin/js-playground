fabbd28ba68b48fc7455b8e46c899a9ff264da7b
Hank Lin <jyunhanlin@gmail.com>
2024-09-10 22:21:03 +0800

    add dp problem

---
+++ leetcode/1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @return {number}
+ */
+var minSumOfLengths = function (arr, target) {
+  const n = arr.length;
+  const dp = new Array(n).fill(Infinity); // dp[i]: the length of smallest subarray from a[0]...a[i] with sum === target
+
+  let result = Infinity;
+  let sum = 0; // sum of current window;
+  let leftIndex = 0;
+  let prevMin = Infinity;
+
+  for (let i = 0; i < n; i += 1) {
+    sum += arr[i];
+
+    while (sum > target) {
+      sum -= arr[leftIndex];
+      leftIndex += 1;
+    }
+
+    if (sum === target) {
+      const curLen = i - leftIndex + 1;
+      result = Math.min(result, curLen + (dp[leftIndex - 1] || Infinity));
+      prevMin = Math.min(prevMin, curLen);
+    }
+    dp[i] = prevMin;
+  }
+
+  return result === Infinity ? -1 : result;
+};
