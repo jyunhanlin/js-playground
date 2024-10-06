c4abad7167141c89a596dc0c7f87e9212fc9af21
Hank Lin <jyunhanlin@gmail.com>
2024-10-06 10:55:15 +0800

    update dp

--- leetcode/2919. Minimum Increment Operations to Make Array Beautiful.js
+++ leetcode/2919. Minimum Increment Operations to Make Array Beautiful.js
@@ -12,7 +12,8 @@ var minIncrementOperations = function (nums, k) {
   dp[2] = k - nums[2] < 0 ? 0 : k - nums[2];
 
   for (let i = 3; i < n; i += 1) {
-    dp[i] = (k - nums[i] <= 0 ? 0 : k - nums[i]) + Math.min(dp[i - 3], dp[i - 2], dp[i - 1]);
+    dp[i] = k - nums[i] < 0 ? 0 : k - nums[i];
+    dp[i] += Math.min(dp[i - 3], dp[i - 2], dp[i - 1]);
   }
 
   return Math.min(dp[n - 1], dp[n - 2], dp[n - 3]);
