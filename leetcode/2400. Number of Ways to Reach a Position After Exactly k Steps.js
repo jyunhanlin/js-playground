09d267f9be6c06c55b3b3968d8beaed78151ebf5
Hank Lin <jyunhanlin@gmail.com>
2024-11-14 21:22:20 +0800

    add dp

---
+++ leetcode/2400. Number of Ways to Reach a Position After Exactly k Steps.js
@@ -0,0 +1,26 @@
+// TLE
+
+/**
+ * @param {number} startPos
+ * @param {number} endPos
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfWays = function (startPos, endPos, k) {
+  const MOD = 1e9 + 7;
+
+  const dp = {};
+  const helper = (pos, curK) => {
+    if (pos === endPos && curK === 0) return 1;
+    if (curK === 0) return 0;
+
+    const key = `${pos}-${curK}`;
+    if (dp[key]) return dp[key];
+
+    dp[key] = (helper(pos + 1, curK - 1) + helper(pos - 1, curK - 1)) % MOD;
+
+    return dp[key];
+  };
+
+  return helper(startPos, k);
+};
