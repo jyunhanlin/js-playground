14e84b50a81fa5ba6ed5d34a6404c83632e502ad
Hank Lin <jyunhanlin@gmail.com>
2024-11-23 10:36:50 +0800

    update dp

--- leetcode/2327. Number of People Aware of a Secret.js
+++ leetcode/2327. Number of People Aware of a Secret.js
@@ -7,6 +7,29 @@
 var peopleAwareOfSecret = function (n, delay, forget) {
   const MOD = 1e9 + 7;
 
+  const dp = new Array(n).fill().map(() => new Array(forget).fill(0));
+
+  dp[0][0] = 1;
+
+  for (let i = 1; i < n; i += 1) {
+    for (let j = 0; j < forget; j += 1) {
+      dp[i][j] += (dp[i - 1][j - 1] || 0) % MOD;
+      if (j - delay >= 0) dp[i][0] += dp[i][j] % MOD;
+    }
+  }
+
+  return dp[n - 1].reduce((a, b) => a + b) % MOD;
+};
+
+/**
+ * @param {number} n
+ * @param {number} delay
+ * @param {number} forget
+ * @return {number}
+ */
+var peopleAwareOfSecret = function (n, delay, forget) {
+  const MOD = 1e9 + 7;
+
   const dp = {};
   const helper = (cur) => {
     if (dp[cur]) return dp[cur];
