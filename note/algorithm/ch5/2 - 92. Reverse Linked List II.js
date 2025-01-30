6c688e45fa33fac881bf15db926484425c01d382
Hank Lin <jyunhanlin@gmail.com>
2025-01-30 11:34:10 +0800

    update algorithm notes

---
+++ note/algorithm/ch5/2 - 92. Reverse Linked List II.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+  if (left === 1) return reverseN(head, right);
+
+  head.next = reverseBetween(head.next, left - 1, right - 1);
+  return head;
+};
+
+let successor = null;
+
+const reverseN = (head, n) => {
+  if (n === 1) {
+    successor = head.next;
+    return head;
+  }
+
+  const last = reverseN(head.next, n - 1);
+  head.next.next = head;
+  head.next = successor;
+
+  return last;
+};
