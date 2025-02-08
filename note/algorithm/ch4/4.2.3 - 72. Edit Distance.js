e501c3df747224e739406561639e0b5ac984d6b1
Hank Lin <jyunhanlin@gmail.com>
2025-02-08 16:14:55 +0800

    update algorithm note

--- note/algorithm/ch4/4.2.3 - 72. Edit Distance.js
+++ note/algorithm/ch4/4.2.3 - 72. Edit Distance.js
@@ -4,20 +4,31 @@
  * @return {number}
  */
 var minDistance = function (word1, word2) {
-  const dp = (i, j) => {
-    if (i === 0) return j + 1;
-    if (j === 0) return i + 1;
+  const m = word1.length;
+  const n = word2.length;
 
+  const memo = new Array(m).fill().map(() => new Array(n).fill(-1));
+
+  const dp = (i, j) => {
+    if (i === -1) return j + 1;
+    if (j === -1) return i + 1;
+
+    if (memo[i][j] !== -1) return memo[i][j];
+
+    let distance;
     if (word1[i] === word2[j]) {
-      return dp(i - 1, j - 1); // do nothing
+      distance = dp(i - 1, j - 1); // do nothing
     } else {
-      return Math.min(
+      distance = Math.min(
         dp(i - 1, j) + 1, // insert
         dp(i, j - 1) + 1, // delete
         dp(i - 1, j - 1) + 1 // replace
       );
     }
+
+    memo[i][j] = distance;
+    return distance;
   };
 
-  return dp(word1.length, word2.length);
+  return dp(m - 1, n - 1);
 };
