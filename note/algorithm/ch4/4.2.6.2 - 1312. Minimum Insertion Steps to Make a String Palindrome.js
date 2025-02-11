601d9717dc0f366f5c68ef16d554c87488e1008e
Hank Lin <jyunhanlin@gmail.com>
2025-02-11 08:26:44 +0800

    update algorithm note

---
+++ note/algorithm/ch4/4.2.6.2 - 1312. Minimum Insertion Steps to Make a String Palindrome.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minInsertions = function (s) {
+  const n = s.length;
+  const dp = new Array(n).fill().map(() => new Array(n).fill(0));
+
+  for (let i = n - 1; i >= 0; i -= 1) {
+    for (let j = i + 1; j < n; j += 1) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minInsertions = function (s) {
+  return s.length - longestPalindromeSubseq(s);
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function (s) {
+  const n = s.length;
+  const dp = new Array(n).fill().map(() => new Array(n).fill(0));
+
+  for (let i = 0; i < n; i += 1) {
+    dp[i][i] = 1;
+  }
+
+  for (let i = n - 1; i >= 0; i -= 1) {
+    for (let j = i + 1; j < n; j += 1) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2;
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+};
