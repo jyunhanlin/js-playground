7d0e3ebf7a41230c814d661a891446227d118e70
Hank Lin <jyunhanlin@gmail.com>
2025-03-08 15:02:13 +0800

    update linked list problem

--- leetcode/86. Partition List.js
+++ leetcode/86. Partition List.js
@@ -43,3 +43,42 @@ var partition = function (head, x) {
 
   return less ? less : ge;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  const dummy1 = new ListNode();
+  const dummy2 = new ListNode();
+  let p1 = dummy1;
+  let p2 = dummy2;
+
+  let p = head;
+
+  while (p) {
+    if (p.val >= x) {
+      p2.next = p;
+      p2 = p2.next;
+    } else {
+      p1.next = p;
+      p1 = p1.next;
+    }
+    const temp = p.next;
+
+    p.next = null;
+    p = temp;
+  }
+
+  p1.next = dummy2.next;
+
+  return dummy1.next;
+};
