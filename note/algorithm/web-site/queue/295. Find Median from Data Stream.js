a6f073b711234d5ec94b5d234765593f59774097
Hank Lin <jyunhanlin@gmail.com>
2025-06-22 10:46:49 +0800

    update queue problem

--- note/algorithm/web-site/queue/295. Find Median from Data Stream.js
+++ note/algorithm/web-site/queue/295. Find Median from Data Stream.js
@@ -8,10 +8,19 @@ var MedianFinder = function () {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function (num) {
+  // Strategy: Always maintain maxPq.size() >= minPq.size()
+  // This ensures that when total count is odd, median is the top of maxPq
+
   if (this.minPq.size() >= this.maxPq.size()) {
+    // Case 1: minPq size >= maxPq size
+    // First add new number to minPq, then move minPq's smallest to maxPq
+    // This ensures maxPq always stores the smaller half of numbers
     this.minPq.enqueue(num);
     this.maxPq.enqueue(this.minPq.dequeue());
   } else {
+    // Case 2: minPq size < maxPq size
+    // First add new number to maxPq, then move maxPq's largest to minPq
+    // This maintains balance between the two heaps
     this.maxPq.enqueue(num);
     this.minPq.enqueue(this.maxPq.dequeue());
   }
