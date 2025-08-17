e989e978e320616a28b4e4118bf408e78cff78db
Hank Lin <jyunhanlin@gmail.com>
2025-08-17 09:13:47 +0800

    update binary tree problems

--- leetcode/897. Increasing Order Search Tree.js
+++ leetcode/897. Increasing Order Search Tree.js
@@ -29,3 +29,36 @@ var increasingBST = function (root) {
 
   return newRoot;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {
+  if (!root) return null;
+
+  const left = increasingBST(root.left);
+  const right = increasingBST(root.right);
+
+  root.left = null;
+  root.right = right;
+
+  if (!left) return root;
+
+  let p = left;
+
+  while (p && p.right) {
+    p = p.right;
+  }
+
+  p.right = root;
+  return left;
+};
