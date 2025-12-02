c9c9dbe537c794ff808fb193d9e3c983e904b4b8
Hank Lin <jyunhanlin@gmail.com>
2025-12-02 08:49:53 +0800

    update sorting problem

--- leetcode/406. Queue Reconstruction by Height.js
+++ leetcode/406. Queue Reconstruction by Height.js
@@ -3,6 +3,7 @@
  * @return {number[][]}
  */
 var reconstructQueue = function (people) {
+  // sort by height descending, if height is the same, sort by k ascending
   people.sort((a, b) => {
     if (a[0] === b[0]) return a[1] - b[1];
     return b[0] - a[0];
@@ -13,6 +14,9 @@ var reconstructQueue = function (people) {
 
   while (sortedIndex < n) {
     for (let i = sortedIndex; i > 0; i -= 1) {
+      // move the i person
+      // the person has i persons in front of the queue
+      // and it's target is k persons in front of the queue
       const k = people[i][1];
       if (k < i) {
         [people[i], people[i - 1]] = [people[i - 1], people[i]];
