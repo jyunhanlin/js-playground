ace29a2707149b735e91626457252f450f601501
Hank Lin <jyunhanlin@gmail.com>
2025-12-05 08:40:03 +0800

    update math problem

--- leetcode/645. Set Mismatch.js
+++ leetcode/645. Set Mismatch.js
@@ -20,3 +20,26 @@ var findErrorNums = function (nums) {
 
   return [dup, missing];
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+  const count = new Array(nums.length + 1).fill(0);
+
+  for (const num of nums) {
+    count[num] += 1;
+  }
+
+  let dup = -1;
+  let missing = -1;
+
+  for (let i = 1; i < count.length; i += 1) {
+    if (count[i] === 2) dup = i;
+
+    if (count[i] === 0) missing = i;
+  }
+
+  return [dup, missing];
+};
