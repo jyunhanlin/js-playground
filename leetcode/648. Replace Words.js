fdb9f8d1741588591641c0a50912784397c4e6ad
Hank Lin <jyunhanlin@gmail.com>
2025-12-07 09:13:24 +0800

    add trie problem

---
+++ leetcode/648. Replace Words.js
@@ -0,0 +1,55 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function (dictionary, sentence) {
+  const trie = new Trie();
+  for (const word of dictionary) {
+    trie.insert(word);
+  }
+
+  const words = sentence.split(' ');
+
+  return words.map((word) => trie.search(word)).join(' ');
+};
+
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEnd = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEnd = true;
+  }
+
+  search(word) {
+    let node = this.root;
+    let prefix = '';
+    for (const char of word) {
+      if (!node.children[char]) {
+        return word;
+      }
+      prefix += char;
+      if (node.children[char].isEnd) {
+        return prefix;
+      }
+      node = node.children[char];
+    }
+    return word;
+  }
+}
