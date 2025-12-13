8db7547d9bd6ef56f1fbdd506b739a6645977c83
Hank Lin <jyunhanlin@gmail.com>
2025-12-13 11:26:20 +0800

    update priority queue problem

--- leetcode/855. Exam Room.js
+++ leetcode/855. Exam Room.js
@@ -98,3 +98,85 @@ ExamRoom.prototype.leave = function (p) {
  * var param_1 = obj.seat()
  * obj.leave(p)
  */
+
+/**
+ * @param {number} n
+ */
+var ExamRoom = function (n) {
+  this.n = n;
+  this.startMap = new Map();
+  this.endMap = new Map();
+  this.intervals = new Set();
+  this.intervals.add([-1, n]);
+};
+
+/**
+ * @return {number}
+ */
+ExamRoom.prototype.seat = function () {
+  let longestInterval;
+  let maxDist = -1;
+
+  for (const interval of this.intervals) {
+    const dist = this.distance(interval);
+
+    if (dist > maxDist || (dist === maxDist && interval[0] < longestInterval[0])) {
+      longestInterval = interval;
+      maxDist = dist;
+    }
+  }
+
+  const [x, y] = longestInterval;
+  let seat;
+
+  if (x === -1) seat = 0;
+  else if (y === this.n) seat = this.n - 1;
+  else seat = Math.floor((y - x) / 2) + x;
+
+  this.removeInterval(longestInterval);
+  this.addInterval([x, seat]);
+  this.addInterval([seat, y]);
+
+  return seat;
+};
+
+/**
+ * @param {number} p
+ * @return {void}
+ */
+ExamRoom.prototype.leave = function (p) {
+  const startFromPInterval = this.startMap.get(p);
+  const endFromPInterval = this.endMap.get(p);
+
+  this.removeInterval(startFromPInterval);
+  this.removeInterval(endFromPInterval);
+  this.addInterval([endFromPInterval[0], startFromPInterval[1]]);
+};
+
+ExamRoom.prototype.addInterval = function (interval) {
+  this.intervals.add(interval);
+  this.startMap.set(interval[0], interval);
+  this.endMap.set(interval[1], interval);
+};
+
+ExamRoom.prototype.removeInterval = function (interval) {
+  this.intervals.delete(interval);
+  this.startMap.delete(interval[0]);
+  this.endMap.delete(interval[1]);
+};
+
+ExamRoom.prototype.distance = function (interval) {
+  const [x, y] = interval;
+  if (x === -1) return y;
+  if (y === this.n) return this.n - 1 - x;
+
+  // the distance from mid to the x
+  return Math.floor((y - x) / 2);
+};
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * var obj = new ExamRoom(n)
+ * var param_1 = obj.seat()
+ * obj.leave(p)
+ */
