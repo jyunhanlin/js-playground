17a429de0d1b53f19ed79769bec67d63955f870a
Hank Lin <jyunhanlin@gmail.com>
2025-12-20 09:26:54 +0800

    add union find problem

---
+++ leetcode/684. Redundant Connection.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findRedundantConnection = function (edges) {
+  const n = edges.length;
+  const uf = new UF(n);
+
+  let result;
+
+  for (const [u, v] of edges) {
+    if (uf.connected(u, v)) {
+      result = [u, v];
+      continue;
+    }
+
+    uf.union(u, v);
+  }
+
+  return result;
+};
+
+class UF {
+  count = 0;
+  parent = [];
+  constructor(n) {
+    this.count = n;
+
+    for (let i = 0; i < n; i += 1) {
+      this.parent[i] = i;
+    }
+  }
+
+  union(p, q) {
+    const rootP = this.find(p);
+    const rootQ = this.find(q);
+
+    if (rootP === rootQ) return;
+
+    this.parent[rootP] = rootQ;
+    this.count -= 1;
+  }
+
+  connected(p, q) {
+    const rootP = this.find(p);
+    const rootQ = this.find(q);
+
+    return rootP === rootQ;
+  }
+
+  find(x) {
+    // while(this.parent[x] !== x) {
+    //   x = this.parent[x];
+    // }
+    // return x;
+
+    // optimize -> compress the height of the union tree
+    if (this.parent[x] !== x) this.parent[x] = this.find(this.parent[x]);
+
+    return this.parent[x];
+  }
+}
